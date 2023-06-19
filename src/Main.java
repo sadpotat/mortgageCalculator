6b49326edcfa217eb23dada84356661b4768768c
sadpotat <sadiatasneem16@gmail.com>
2023-06-19 23:46:28 +0600

    added input validation

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,32 @@ public class Main {
         // getting values
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
 
-        System.out.print("Annual interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        float annualInterest;
+        while (true) {
+            System.out.print("Annual interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0f && annualInterest <= 30f)
+                break;
+            System.out.println("Enter a number greater than 0 and less than 30");
+        }
 
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-
+        byte period;
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scanner.nextByte();
+            if (period >= 1 && period <= 30)
+                break;
+            System.out.println("Enter a number greater than 1 and less than 30");
+        }
 
         // calculations
         float monthlyInterest = annualInterest/1200;
